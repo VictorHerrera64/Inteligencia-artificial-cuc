d44d0c27797feaccc3ecc04913c8d1a975199047
Victor Herrera <pasiondelbarza@gmail.com>
2022-02-23 20:48:47 -0500

    se agrego ejercicios de python cuc

---
+++ Introduccion/Ejerccios/clase-1-python.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#ejercicio 1
+valor = int(input('Ingresa el valor de un producto: '))
+ahorro = valor * 0.2
+valor_descuento = valor - ahorro
+print(f"El valor inicial del producto es: {valor}, el valor con descuento es: {valor_descuento} y el valor ahorrado es: {ahorro}")
+
+#ejercicios 2
+num1 = int(input("Ingresa el primer número: "))
+num2 = int(input("Ingresa otro número: "))
+suma = num1 + num2
+print(f"{num1} + {num2} = {suma}")
